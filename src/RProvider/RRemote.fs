97c351d7bb5eccc135f2502679dac6d1188180f0
David Charboneau <david@adadabase.com>
2014-04-10 22:10:28 -0400

    Add a simple remote R session class
    This provides a simple wrapper class for interacting with a remote R
    shell through svSocket

---
+++ src/RProvider/RRemote.fs
@@ -0,0 +1,33 @@
+﻿namespace RProvider
+
+open Microsoft.FSharp.Core.CompilerServices
+open RDotNet
+open RInterop
+open Samples.FSharp.ProvidedTypes
+open System
+
+type public RRemoteSession(connectionName) =
+    
+    static member Connect connectionName host port blocking =
+        let require = RInteropInternal.eval("require(svSocket)")
+        let blockingString =
+            match blocking with
+            | true -> "TRUE"
+            | false -> "FALSE"
+        let connectionExpr =
+            sprintf "socketConnection(host=\"%s\",port=\"%d\",blocking=%s)" host port blockingString
+        RInteropInternal.evalTo connectionExpr connectionName
+
+    static member GetConnection host port blocking =
+        let suffix = System.Random().Next()
+        let connectionName = sprintf "con%d" suffix
+        RRemoteSession.Connect connectionName host port blocking
+        new RRemoteSession(connectionName)
+
+    member this.connectionName = connectionName
+
+    member this.eval expr =
+        RInteropInternal.eval(sprintf "evalServer(%s,\"%s\")" this.connectionName expr)
+
+    member this.evalTo expr symbol =
+        this.eval(sprintf "%s <- %s" symbol expr)
